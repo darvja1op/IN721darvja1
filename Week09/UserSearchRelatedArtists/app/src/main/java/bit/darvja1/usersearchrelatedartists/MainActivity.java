928bb2336aaa66e7b86cd118939cda8283e1ea2c
Jared <jared.darvilljackson@gmail.com>
2016-04-15 16:32:38 +1200

    UserSearch

--- Week09/UserSearchRelatedArtists/app/src/main/java/bit/darvja1/usersearchrelatedartists/MainActivity.java
+++ Week09/UserSearchRelatedArtists/app/src/main/java/bit/darvja1/usersearchrelatedartists/MainActivity.java
@@ -1,15 +1,15 @@
-package bit.darvja1.lastfmhttp;
+package bit.darvja1.usersearchrelatedartists;
 
 import android.content.Context;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,10 +17,8 @@ import android.widget.Toast;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -30,6 +28,8 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     ListView listView;
     ArrayList<String> artistsList;
+    String artist;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,21 +39,24 @@ public class MainActivity extends AppCompatActivity {
         Button btnFill = (Button) findViewById(R.id.btnFill);
         btnFill.setOnClickListener(new ButtonClick());
 
+        artist = "";
         artistsList = new ArrayList<String>();
         listView = (ListView) findViewById(R.id.listView);
+        editText = (EditText) findViewById(R.id.editText);
     }
 
     private class ButtonClick implements View.OnClickListener{
 
         @Override
         public void onClick(View v) {
+            artist = editText.getText().toString();
             AsyncAPI APIThread = new AsyncAPI();
-            APIThread.execute();
+            APIThread.execute(artist);
         }
     }
 
     public void fillList(){
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.custom_list,artistsList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,artistsList);
         listView.setAdapter(adapter);
     }
 
@@ -61,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         JSONObject JSON = null;
         try {
             JSON = new JSONObject(JSONString);
-            JSONObject object = JSON.getJSONObject("artists");
+            JSONObject object = JSON.getJSONObject("similarartists");
 
             JSONArray artists = object.getJSONArray("artist");
 
@@ -70,11 +73,8 @@ public class MainActivity extends AppCompatActivity {
             for (int i = 0; i<arrayLength; i++){
                 JSONObject artist = artists.getJSONObject(i);
                 String name = artist.getString("name");
-                int playcount = artist.getInt("playcount");
 
-                String text = name + " " + playcount;
-
-                artistsList.add(text);
+                artistsList.add(name);
             }
         } catch (JSONException e) {
             e.printStackTrace();
@@ -83,20 +83,20 @@ public class MainActivity extends AppCompatActivity {
         fillList();
     }
 
-    private class AsyncAPI extends AsyncTask<Void,Void,String>{
+    private class AsyncAPI extends AsyncTask<String,Void,String> {
 
         @Override
         protected void onPreExecute(){
         }
 
         @Override
-        protected String doInBackground(Void... arg0) {
+        protected String doInBackground(String... artist) {
             String JSONString = null;
 
             try{
                 String urlString = "http://ws.audioscrobbler.com/2.0/"+
-                        "?method=chart.getTopArtists&"+
-                        "api_key=58384a2141a4b9737eacb9d0989b8a8c&limit=20&format=json";
+                        "?method=artist.getSimilar&artist="+artist[0]+
+                        "api_key=58384a2141a4b9737eacb9d0989b8a8c&limit=10&format=json";
 
                 URL URLObject = new URL(urlString);
                 HttpURLConnection connection = (HttpURLConnection) URLObject.openConnection();
@@ -106,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
 
                 if (responseCode != 200){
                     //no data
-                    Toast.makeText(MainActivity.this,"No Data found",Toast.LENGTH_LONG);
+                    Toast.makeText(MainActivity.this, "No Data found", Toast.LENGTH_LONG).show();
                 }
 
                 InputStream inputStream = connection.getInputStream();
@@ -131,27 +131,6 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String fetchedString){
             //pass string to appropriate place
             processJSON(fetchedString);
-        }
-
-        public class CustomArrayAdapter extends ArrayAdapter<String>{
-
-            public CustomArrayAdapter(Context context, int resource) {
-                super(context, resource);
-            }
-
-            @Override
-            public View getView(int position, View convertView, ViewGroup container){
-                LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
-
-                View customView = inflater.inflate(R.layout.custom_list, container, false);
-
-                TextView txtView = (TextView) customView.findViewById(R.id.textView);
-                TextView txtView2 = (TextView) customView.findViewById(R.id.textView2);
-
-
-
-                return customView;
-            }
         }
     }
 }
