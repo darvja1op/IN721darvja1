925c0fe9c67a77e84fa499fc5ac0f889edd5d45d
Jared <jared.darvilljackson@gmail.com>
2016-05-05 20:07:32 +1200

    Task 1 Complete
    need to adjust rounding to 3d.p

--- Week10/LocationTeleporter/app/src/main/java/bit/darvja1/locationteleporter/MainActivity.java
+++ Week10/LocationTeleporter/app/src/main/java/bit/darvja1/locationteleporter/MainActivity.java
@@ -2,6 +2,11 @@ package bit.darvja1.locationteleporter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnTeleport = (Button) findViewById(R.id.btnGenerate);
+        btnTeleport.setOnClickListener(new ButtonListener());
+    }
+
+    private class ButtonListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            generateCoordinates();
+        }
+    }
+
+    public void generateCoordinates(){
+        Random rand = new Random();
+        double doubleGen;
+        int intGen;
+
+        doubleGen = rand.nextDouble();
+        intGen = rand.nextInt(180) - 89;
+        double latitude = doubleGen + intGen;
+
+        doubleGen = rand.nextDouble();
+        intGen = rand.nextInt(360) - 179;
+        double longitude = doubleGen + intGen;
+
+        displayCoordinates(latitude, longitude);
+    }
+
+    public void displayCoordinates(double latitude, double longitude){
+        TextView txtLatitude = (TextView) findViewById(R.id.txtLatitudeDisplay);
+        TextView txtLongitude = (TextView) findViewById(R.id.txtLongitudeDisplay);
+
+        txtLatitude.setText(Double.toString((double)Math.round(latitude * 100000d) / 100000d));
+        txtLongitude.setText(Double.toString((double)Math.round(longitude * 100000d) / 100000d));
     }
 }
